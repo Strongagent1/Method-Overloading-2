0fd22f9bc81c485c5deebc7fab4bd8a716f36cee
Strongagent1 <115094376+Strongagent1@users.noreply.github.com>
2023-01-04 10:56:06 -0500

    Completed Method Overloading exercise again KJ

---
+++ Method-Overloading-Exercise-/MethodOverloading/AddMethods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    public class AddMethods
+    {
+
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static decimal Addition(decimal a, decimal b)
+        {
+            return a + b;
+        }
+        public static string Addition(int a, int b, bool isTrue)
+        {
+            var sum = a + b;
+
+            if (isTrue == true && sum >= 2)
+            {
+                return $"{sum} dollars.";
+            }
+            else
+            {
+                return $" {sum} dollar.";
+
+
+
+            }
+
+
+        }
+
+    }
+}
+
---
+++ Method-Overloading-Exercise-/MethodOverloading/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MethodOverloading
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 29;
+            var b = 55;
+
+            int answer = AddMethods.Addition( a,  b);
+
+           
+
+            var decimalAnswer = AddMethods.Addition(a, b);
+
+            var thirdAnswer = AddMethods.Addition(0, 1, true);
+
+            Console.WriteLine($"int add: {answer} decimal add: {decimalAnswer}");
+            Console.WriteLine(thirdAnswer);
+        }
+    }
+}
